a782cfb16b913df0472bb0b4f3d8d6adf4f5f022
Joaquín Navarro <joaquin.navarro.wks@gmail.com>
2023-08-05 19:07:32 -0300

    cambie la funcionalidad del boton

--- eventos.js
+++ eventos.js
@@ -1,6 +1,9 @@
-function saludar() {
+let boton = document.getElementById("boton-saludador");
+boton.addEventListener("click", function () {
+  event.stopPropagation();
   alert("Hola!");
-}
+});
+
 let container = document.getElementById("button-container");
 container.addEventListener("click", function () {
   alert("Hola, soy el div!");
