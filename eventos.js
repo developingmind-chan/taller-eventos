ab56a92bd3023a8cafa07b88297eba947e6b07ef
Joaquín Navarro <joaquin.navarro.wks@gmail.com>
2023-08-05 18:32:20 -0300

    div creado, personalizado y eventuado con click

---
+++ eventos.js
@@ -0,0 +1,7 @@
+function saludar() {
+  alert("Hola!");
+}
+let container = document.getElementById("button-container");
+container.addEventListener("click", function () {
+  alert("Hola, soy el div!");
+});
